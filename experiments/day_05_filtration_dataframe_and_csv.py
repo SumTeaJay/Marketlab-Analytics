a220d734f3137fe0b1dc1c3421244aee6958934f
Sum Tea Jay <112896792+SumTeaJay@users.noreply.github.com>
2026-09-02 13:07:40 +0300

    День 5

---
+++ experiments/day_05_filtration_dataframe_and_csv.py
@@ -0,0 +1,16 @@
+from generate_markets import *
+
+arrays = generate_market_parameters(100)
+
+df = create_data_frame(arrays[0], arrays[1], arrays[2])
+
+mean_price = np.mean(df["price"])
+df_markets_higher_price = df[df["price"] >= mean_price]
+
+df_markets_higher_price = df_markets_higher_price.sort_values("price")
+
+df_markets_higher_price.to_csv(r"data\markets.csv")
+
+df = pd.read_csv(r"data\markets.csv", index_col="market_id")
+
+print(df)
--- generate_markets.py
+++ generate_markets.py
@@ -5,29 +5,21 @@ def generate_market_parameters(count=1, seed=42):
     rng = np.random.default_rng(seed)
 
     a = rng.integers(1, 1000, size=count)
-    b = rng.integers(1, 1000, size=count)
-    Q = rng.integers(1, 1000, size=count)
+    b = rng.integers(1, 10, size=count)
+    Q = rng.integers(1, 10, size=count)
 
     return (a, b, Q)
 
 def return_prices(a: np.array, b: np.array, Q: np.array) -> np.array:
     P = a - b * Q
-    only_non_negative = P >= 0
-    return P[only_non_negative]
+    return P[P >= 0]
 
-def convert_to_data_frame(a: np.array, b: np.array, Q: np.array) -> pd.DataFrame:
+def create_data_frame(a: np.array, b: np.array, Q: np.array) -> pd.DataFrame:
     df = pd.DataFrame({
-        "a": a,
-        "b": b,
-        "Q": Q,
+        "demand_intercept": a,
+        "demand_slope": b,
+        "quantity": Q,
     }, index = list(range(1, len(a) + 1)))
+    df["price"] = return_prices(a, b, Q)
     df.index.name = "market_id"
     return df
-
-
-def main():
-    arrays = generate_market_parameters(100)
-    print(convert_to_data_frame(arrays[0], arrays[1], arrays[2]))
-
-if __name__ == "__main__":
-    main()

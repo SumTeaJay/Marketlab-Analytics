833c86aa62641d63f365385972b9cf3e185e6963
Sum Tea Jay <112896792+SumTeaJay@users.noreply.github.com>
2026-09-10 09:07:50 +0300

    Расчеты дня 8 и пустой файл для анализа рынков

---
+++ experiments/day_08_variational_series.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv(r"data\processed\markets_clean.csv", index_col="market_id")
+
+print(df["quantity"].value_counts(normalize=True))
+print("#" * 32)
+print(df["quantity"].value_counts(normalize=False))
+print("#" * 32)
+print(df["quantity"].value_counts(normalize=True).sum())
+print("#" * 32)
+print(df["price"].agg(["mean", "median", "var", "std"]).round(2))
+print("#" * 32)
+df_with_higher_prices = df[df["price"] > df["price"].mean()]
+print(len(df_with_higher_prices) / len(df) * 100)
+print("#" * 32)
+print(df.groupby("demand_slope")["price"].agg(["count", "mean", "median"]).round(2))

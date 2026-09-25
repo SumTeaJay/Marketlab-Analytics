5d82e1afebe06395f9c79b07f19181eafdff7f05
Sum Tea Jay <112896792+SumTeaJay@users.noreply.github.com>
2026-09-25 15:44:32 +0300

    Единая аналитическая таблица, новый график, анализ dwl через demand_slope

--- app/analysis.py
+++ app/analysis.py
@@ -3,21 +3,8 @@ import matplotlib.pyplot as plt
 
 #pc - perfect competition
 
-def create_united_df(pc: pd.DataFrame, monopoly: pd.DataFrame) -> pd.DataFrame:
-    pc["regime"] = "Совершенная конкуренция"
-    monopoly["regime"] = "Монополия"
 
-    column = pc.pop('regime')
-    pc.insert(0, 'regime', column)
-
-    column = monopoly.pop('regime')
-    monopoly.insert(0, 'regime', column)
-
-    new_united_df = pd.concat([pc, monopoly])
-    new_united_df = new_united_df.sort_values(by=["market_id", "regime"])
-    new_united_df["difference_between_a_and_MC"] = new_united_df["demand_intercept"] - new_united_df["marginal_costs"]
-    return new_united_df
-
+### Все функции под этим комментарием до create_report_1 относятся к отчету №1
 def compare_prices(monopoly: pd.DataFrame, pc: pd.DataFrame):
     mean_difference = (monopoly["price"] - pc["price"]).mean()
     median_difference = (monopoly["price"] - pc["price"]).median()
@@ -58,7 +45,7 @@ def create_graph_price_difference(monopoly: pd.DataFrame, pc: pd.DataFrame):
 
     plt.savefig(r"data\graphs\price_difference.png")    
 
-def create_graph_dwl_connection_with_price(monopoly: pd.DataFrame):
+def create_graph_dwl_connection_with_quantity(monopoly: pd.DataFrame):
     plt.scatter(
         monopoly["quantity"],
         monopoly["dwl"]
@@ -70,6 +57,19 @@ def create_graph_dwl_connection_with_price(monopoly: pd.DataFrame):
     plt.title("Связь между равновесным количеством и общественными потерями")
 
     plt.savefig(r"data\graphs\dwl_connection_with_quantity.png")
+
+def create_graph_dwl_connection_with_ps(dataframe: pd.DataFrame):
+    plt.scatter(
+        dataframe["ps"],
+        dataframe["dwl"]
+    )
+
+    plt.xlabel("Излишек производителя")
+    plt.ylabel("Общественные потери")
+
+    plt.title("Связь между излишком производителя и общественными потерями")
+
+    plt.savefig(r"data\graphs\dwl_connection_with_ps.png")
 
 def create_report_1(pc: pd.DataFrame, monopoly: pd.DataFrame):
     with open(r"data\reports\report_1.txt", "w", encoding="utf-8") as report_file:
@@ -104,15 +104,46 @@ def create_report_1(pc: pd.DataFrame, monopoly: pd.DataFrame):
         print(f"Средние общественные потери при монополии: {dwl[0]}", file=report_file)
         print(f"Медианные общественные потери при монополии: {dwl[1]}", file=report_file)
 
-def create_report_2(pc: pd.DataFrame, monopoly: pd.DataFrame):
-    pass
+
+### Все функции под этим комментарием до create_report_2 относятся к отчету №2
+def create_united_df(pc: pd.DataFrame, monopoly: pd.DataFrame) -> pd.DataFrame:
+    pc["regime"] = "Совершенная конкуренция"
+    monopoly["regime"] = "Монополия"
+
+    column = pc.pop('regime')
+    pc.insert(0, 'regime', column)
+
+    column = monopoly.pop('regime')
+    monopoly.insert(0, 'regime', column)
+
+    new_united_df = pd.concat([pc, monopoly])
+    new_united_df = new_united_df.sort_values(by=["market_id", "regime"])
+    new_united_df["difference_between_a_and_MC"] = new_united_df["demand_intercept"] - new_united_df["marginal_costs"]
+    return new_united_df
+
+def groupby_b_and_analyze_dwl(united_dataframe: pd.DataFrame):
+    united_dataframe = united_dataframe.groupby(by="demand_slope")
+    summary = united_dataframe.agg(count_of_markets = ("regime", "count"), mean_dwl = ("dwl", "mean"), median_dwl = ("dwl", "median"), mean_difference_between_a_and_MC = ("difference_between_a_and_MC", "mean"), min_dwl = ("dwl", "min"), max_dwl = ("dwl", "max"))
+    return summary
+
+def add_mean_dwl(united_dataframe: pd.DataFrame) -> pd.DataFrame:
+    united_dataframe["mean_dwl"] = united_dataframe.groupby("demand_slope")["dwl"].transform("mean")
+    return united_dataframe
+
+
+def create_report_2(united_dataframe: pd.DataFrame):
+    united_dataframe.groupby(by="demand_slope")
+    summary = groupby_b_and_analyze_dwl(united_dataframe)
+    united_dataframe = add_mean_dwl(united_dataframe)
+    united_dataframe.to_csv(r"data\reports\report_2_analytical_dataframe.csv")
+    summary.to_csv(r"data\reports\report_2_summary.csv")
+    create_graph_dwl_connection_with_ps(united_dataframe)
 
 def analyze_markets():
     monopoly = pd.read_csv(r"data\calculated_data\monopoly.csv", index_col="market_id")
     pc = pd.read_csv(r"data\calculated_data\perfect_competition.csv", index_col="market_id")
-    print(create_united_df(monopoly, pc))
-
-    create_report_1(pc, monopoly)
-    create_report_2(pc, monopoly)
+    united_df = create_united_df(pc, monopoly)
+    # create_report_1(pc, monopoly)
+    create_report_2(united_df)
 
 
